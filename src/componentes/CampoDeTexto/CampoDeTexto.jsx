4a53de901df72bcf3d5138b7c7518203e7286538
Gabriel Siqueira <gabriellopes1010@gmail.com>
2023-11-20 14:47:18 -0300

    criando componentes de tipografia e campo texto

---
+++ src/componentes/CampoDeTexto/CampoDeTexto.jsx
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled"
+
+const LabelEstilizada = styled.label`
+    display: block;
+    width: 100%
+    box-sizing: border-box;
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 24px;
+`
+
+const InputEstilizado = styled.input`
+    display: block;
+    box-sizing: border-box;
+    margin-top: ${props => props.theme.espacamentos.xs};
+    width: 100%;
+    background: ${props => props.theme.cores.branco};
+    border: 1px solid ;
+    border-color: ${props => props.theme.cores.neutras.a};
+    border-radius: ${props => props.theme.espacamentos.s};
+    height: 40px;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 17px;
+`
+
+export const CampoTexto = ({ titulo }) => {
+    
+   return (<LabelEstilizada>
+        {titulo}
+        <InputEstilizado />
+    </LabelEstilizada>)
+}
